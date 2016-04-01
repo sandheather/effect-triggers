f2701c8bd4bdfa92a5ee3cb5cca7f8f27dba7a5f
sandheather <heather.e.sanders22@gmail.com>
2016-04-01 12:20:07 -0400

    spaces deleted and added

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
  $('.btn-show-hide').on('click', function () {
-  $('.box').toggleClass('show');
-});
+   $('.box').toggleClass('show');
+ });
 
-  $('.btn-move').on('click', function () {
-  	$('.diamond').toggleClass('slide');
-});
+ $('.btn-move').on('click', function () {
+   $('.diamond').toggleClass('slide');
+ });
 
-  $('.btn-collapse-expand').on('click', function () {
-  	$('.panel').toggleClass('collapse');
-});
+ $('.btn-collapse-expand').on('click', function () {
+   $('.panel').toggleClass('collapse');
+ });
 
-  $('.btn-bounce').on('click', function () {
-  	$('.circle').addClass('bounce');
-});
+ $('.btn-bounce').on('click', function () {
+   $('.circle').addClass('bounce');
+ });
 
-  $('.circle').on('click', function () {
-  	$('.circle').removeClass('bounce');
-});
+ $('.circle').on('click', function () {
+   $('.circle').removeClass('bounce');
+ });
 
-  $('.btn-append').on('click', function () {
-  $('.list').toggleClass('<li>New List Item</li>');
-});
+ $('.btn-append').on('click', function () {
+   $('.list').toggleClass('<li>New List Item</li>');
+ });
